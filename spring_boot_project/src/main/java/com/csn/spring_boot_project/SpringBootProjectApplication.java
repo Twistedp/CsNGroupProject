27d01d1cdeb140c71e4c9faff1b27c9fdbc45d89
Magreken <mgrebien01@gmail.com>
2024-01-17 17:40:11 +0100

    Register user update

--- spring_boot_project/src/main/java/com/csn/spring_boot_project/SpringBootProjectApplication.java
+++ spring_boot_project/src/main/java/com/csn/spring_boot_project/SpringBootProjectApplication.java
@@ -2,8 +2,10 @@ package com.csn.spring_boot_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class SpringBootProjectApplication {
 
 	public static void main(String[] args) {
